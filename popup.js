a93074fcd3854590009fef7a067e38a31128ca1c
Broast <blankx@gmail.com>
2025-07-13 21:52:49 -0400

    Task attempt aa56bd9a-bfb6-48aa-b960-2f3a0109c984 - Final changes

--- popup.js
+++ popup.js
@@ -44,6 +44,11 @@ class PopupManager {
                         panel.style.display = 'none';
                     }
                 });
+
+                const advancedContent = document.getElementById("advanced-options-content");
+                if (advancedContent.style.maxHeight) {
+                    advancedContent.style.maxHeight = advancedContent.scrollHeight + "px";
+                }
             });
         });
     }
